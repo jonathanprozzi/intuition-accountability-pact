23f005c2a01674139b4a2152139014b96eb924cb
jonathanprozzi <jonathanprozzi@gmail.com>
2023-11-01 15:59:32 -0400

    adds temporary workaround for dynamic wallet address validation -- will revisit

--- app/routes/app.create-pact._index.tsx
+++ app/routes/app.create-pact._index.tsx
@@ -32,6 +32,9 @@ export async function loader({ request }: LoaderFunctionArgs) {
 const addressRegex = /^0x[a-fA-F0-9]{40}$/
 const createDyamicValidationSchema = ({ wallet }: { wallet: string }) => {
   return z.object({
+    userAddress: z
+      .string()
+      .regex(addressRegex, { message: 'Invalid Ethereum address format.' }),
     accountabilityAddress: z
       .string({ required_error: 'Accountability Address is required.' })
       .regex(addressRegex, { message: 'Invalid Ethereum address format.' })
@@ -76,6 +79,7 @@ export default function CreatePactIndexRoute() {
 // @TODO: pass in the address in a hidden field instead of loading it in via the loader
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData()
+  console.log('formdata', formData)
   const dynamicValidationSchema = createDyamicValidationSchema({
     wallet: '0x25709998B542f1Be27D19Fa0B3A9A67302bc1b94',
   })
@@ -87,7 +91,7 @@ export async function action({ request }: ActionFunctionArgs) {
     return json(submission)
   }
 
-  return redirect(`/?value=${JSON.stringify(submission.value)}`)
+  return redirect(`/app?value=${JSON.stringify(submission.value)}`)
 }
 
 export function CreatePactForm() {
@@ -95,13 +99,14 @@ export function CreatePactForm() {
   const dynamicValidationSchema = createDyamicValidationSchema({
     wallet: '0x25709998B542f1Be27D19Fa0B3A9A67302bc1b94',
   })
-  const [form, { accountabilityAddress, pactDescription }] = useForm({
-    lastSubmission,
-    onValidate({ formData }) {
-      return parse(formData, { schema: dynamicValidationSchema })
-    },
-    shouldValidate: 'onBlur',
-  })
+  const [form, { userAddress, accountabilityAddress, pactDescription }] =
+    useForm({
+      lastSubmission,
+      onValidate({ formData }) {
+        return parse(formData, { schema: dynamicValidationSchema })
+      },
+      shouldValidate: 'onBlur',
+    })
   return (
     <Card className="w-full pb-8 pt-4">
       <div className="space-y-4">
@@ -110,6 +115,14 @@ export function CreatePactForm() {
           {...form.props}
           className=" flex flex-col gap-4 p-6"
         >
+          {/* <div className="flex flex-col gap-2">
+            <Label />
+            <Input
+              data-hidden={true}
+              {...conform.input(userAddress)}
+              className="data-[hidden=true]:hidden"
+            />
+          </div> */}
           <div className="flex flex-col gap-2">
             <Label className="m-x-auto text-sm text-foreground">
               Accountability Address
