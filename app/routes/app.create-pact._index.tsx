5987898481a40e6ad54b079af8c76189fb577213
jonathanprozzi <jonathanprozzi@gmail.com>
2023-11-01 19:43:07 -0400

    adds new structure -- prevents the submit and then has room for a client side async function before submit

--- app/routes/app.create-pact._index.tsx
+++ app/routes/app.create-pact._index.tsx
@@ -1,9 +1,15 @@
-import react from 'react'
+import react, { useRef } from 'react'
 import { conform, useForm } from '@conform-to/react'
 import { parse } from '@conform-to/zod'
 import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node'
 import { json, redirect } from '@remix-run/node'
-import { Form, Link, useActionData, useLoaderData } from '@remix-run/react'
+import {
+  Form,
+  Link,
+  useActionData,
+  useLoaderData,
+  useSubmit,
+} from '@remix-run/react'
 import { z } from 'zod'
 import { Button as BaseButton, Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
@@ -30,22 +36,6 @@ export async function loader({ request }: LoaderFunctionArgs) {
 // test address: 0x04EA475026a0AB3e280F749b206fC6332E6939F1
 
 const addressRegex = /^0x[a-fA-F0-9]{40}$/
-const createDyamicValidationSchema = ({ wallet }: { wallet: string }) => {
-  return z.object({
-    userAddress: z
-      .string()
-      .regex(addressRegex, { message: 'Invalid Ethereum address format.' }),
-    accountabilityAddress: z
-      .string({ required_error: 'Accountability Address is required.' })
-      .regex(addressRegex, { message: 'Invalid Ethereum address format.' })
-      .refine((walletAddress) => walletAddress !== wallet, {
-        message: `Accountability Address can't be the same as your wallet address.`,
-      }),
-    pactDescription: z.string({
-      required_error: 'Pact Description is required.',
-    }),
-  })
-}
 
 const validationSchema = z.object({
   accountabilityAddress: z
@@ -79,13 +69,9 @@ export default function CreatePactIndexRoute() {
 // @TODO: pass in the address in a hidden field instead of loading it in via the loader
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData()
-  console.log('formdata', formData)
-  const dynamicValidationSchema = createDyamicValidationSchema({
-    wallet: '0x25709998B542f1Be27D19Fa0B3A9A67302bc1b94',
-  })
-
+  console.log('submit action calling')
   console.log('submit action', formData)
-  const submission = parse(formData, { schema: dynamicValidationSchema })
+  const submission = parse(formData, { schema: validationSchema })
 
   if (!submission.value || submission.intent !== 'submit') {
     return json(submission)
@@ -94,35 +80,62 @@ export async function action({ request }: ActionFunctionArgs) {
   return redirect(`/app?value=${JSON.stringify(submission.value)}`)
 }
 
+const asyncConsoleLog = (value: string): Promise<void> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(value)
+      resolve()
+    }, 1000) // waits 1 second before logging and resolving the promise
+  })
+}
+
+const createPactSplit = async ({
+  accountabilityAddress,
+}: {
+  accountabilityAddress: FormDataEntryValue | null | string
+}) => {
+  await asyncConsoleLog(
+    `in createPactSplit: accountabilityAddress: ${accountabilityAddress}`,
+  )
+}
+
 export function CreatePactForm() {
   const lastSubmission = useActionData<typeof action>()
-  const dynamicValidationSchema = createDyamicValidationSchema({
-    wallet: '0x25709998B542f1Be27D19Fa0B3A9A67302bc1b94',
+  const formRef = useRef<HTMLFormElement>(null)
+  const [form, { accountabilityAddress, pactDescription }] = useForm({
+    lastSubmission,
+    onValidate({ formData }) {
+      return parse(formData, { schema: validationSchema })
+    },
+    shouldValidate: 'onBlur',
   })
-  const [form, { userAddress, accountabilityAddress, pactDescription }] =
-    useForm({
-      lastSubmission,
-      onValidate({ formData }) {
-        return parse(formData, { schema: dynamicValidationSchema })
-      },
-      shouldValidate: 'onBlur',
-    })
+
+  const createPactSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    const submit = useSubmit()
+    event.preventDefault()
+    const form = event.target as HTMLFormElement
+    const formData = new FormData(form)
+    const accountabilityAddress = formData.get('accountabilityAddress')
+    const pactDescription = formData.get('pactDescription')
+    console.log(
+      'in createPactSubmit: accountabilityAddress',
+      accountabilityAddress,
+    )
+    // await createPactSplit({ accountabilityAddress })
+    // formRef.current?.requestSubmit() // This will invoke the Remix action
+    submit(event.currentTarget, { replace: true })
+  }
+
   return (
     <Card className="w-full pb-8 pt-4">
       <div className="space-y-4">
         <Form
           method="post"
           {...form.props}
+          ref={formRef}
+          onSubmit={createPactSubmit}
           className=" flex flex-col gap-4 p-6"
         >
-          {/* <div className="flex flex-col gap-2">
-            <Label />
-            <Input
-              data-hidden={true}
-              {...conform.input(userAddress)}
-              className="data-[hidden=true]:hidden"
-            />
-          </div> */}
           <div className="flex flex-col gap-2">
             <Label className="m-x-auto text-sm text-foreground">
               Accountability Address
@@ -141,7 +154,12 @@ export function CreatePactForm() {
               {pactDescription.error}
             </span>
           </div>
-          <Button variant="outline" size="sm" className="block w-full">
+          <Button
+            variant="outline"
+            size="sm"
+            className="block w-full"
+            type="submit"
+          >
             Create Pact
           </Button>
         </Form>
