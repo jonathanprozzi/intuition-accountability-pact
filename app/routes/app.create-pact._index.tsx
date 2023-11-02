c6302aebd8a731d37af402657417b51eefe10bdd
jonathanprozzi <jonathanprozzi@gmail.com>
2023-11-02 03:49:39 -0400

    switched to wagmi/actions, getting the txHash now

--- app/routes/app.create-pact._index.tsx
+++ app/routes/app.create-pact._index.tsx
@@ -27,15 +27,23 @@ import { AccountButton } from '@/components/account-button'
 import {
   usePrepareSendTransaction,
   useSendTransaction,
+  useTransaction,
   useWaitForTransaction,
 } from 'wagmi'
+import {
+  sendTransaction,
+  prepareSendTransaction,
+  waitForTransaction,
+} from '@wagmi/core'
 import Header from '@/components/header'
-import { requireAuthedUser } from '@/lib/services/auth.server'
+import { login, requireAuthedUser } from '@/lib/services/auth.server'
 import { User } from 'types/user'
 import { Textarea } from '@/components/ui/textarea'
 
 import { parseEther } from 'viem'
-import { sendTransaction } from 'viem/actions'
+import useClientTransaction from '@/lib/utils/useClientTransaction'
+import { formAction } from '@/lib/services/form.server'
+import { makeDomainFunction } from 'domain-functions'
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const user = (await requireAuthedUser(request)) as User
@@ -53,27 +61,42 @@ const validationSchema = z.object({
   userAddress: z
     .string({ required_error: 'Accountability Address is required.' })
     .regex(addressRegex, { message: 'Invalid Ethereum address format.' }),
-  accountabilityAddress: z
-    .string({ required_error: 'Accountability Address is required.' })
-    .regex(addressRegex, { message: 'Invalid Ethereum address format.' }),
-  pactDescription: z.string({
-    required_error: 'Pact Description is required.',
-  }),
+  // accountabilityAddress: z
+  //   .string({ required_error: 'Accountability Address is required.' })
+  //   .regex(addressRegex, { message: 'Invalid Ethereum address format.' }),
+  // pactDescription: z.string({
+  //   required_error: 'Pact Description is required.',
+  // }),
 })
 
-export async function action({ request }: ActionFunctionArgs) {
-  const formData = await request.formData()
-  const submission = parse(formData, { schema: validationSchema })
+// export async function action({ request }: ActionFunctionArgs) {
+//   const formData = await request.formData()
+//   const submission = parse(formData, { schema: validationSchema })
 
-  /**
-   * Signup only when the user click on the submit button and no error found
-   */
-  if (!submission.value || submission.intent !== 'submit') {
-    // Always sends the submission state back to client until the user is signed up
-    return json(submission)
+//   if (!submission.value || submission.intent !== 'submit') {
+//     return json(submission)
+//   }
+//   console.log('submission values', JSON.stringify(submission.payload))
+//   return redirect(
+//     `/app/create-pact?value=${JSON.stringify(submission.payload)}`,
+//   )
+// }
+
+const mutation = makeDomainFunction(validationSchema)(async (values) => {
+  return values
+})
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const resp = await formAction({
+    request,
+    schema: validationSchema,
+    mutation,
+  })
+  if (resp.ok) {
+    // await login(request)
+    console.log('resp', resp)
   }
-
-  return redirect(`/?value=${JSON.stringify(submission.value)}`)
+  return null
 }
 
 export default function CreatePactIndexRoute() {
@@ -99,75 +122,120 @@ export default function CreatePactIndexRoute() {
 
 export function CreatePactForm() {
   const { wallet } = useLoaderData<typeof loader>()
-  const fetcher = useFetcher<typeof action>()
-
-  const [txHash, setTxHash] = useState<`0x${string}` | null>(null)
-  const [isTxLoading, setIsTxLoading] = useState(false)
-  const [isSubmitting, setIsSubmitting] = useState(false)
-
-  const { config } = usePrepareSendTransaction({
-    to: '0x04EA475026a0AB3e280F749b206fC6332E6939F1',
-    value: parseEther('0.00001'),
-  })
-  const { data, sendTransaction } = useSendTransaction(config)
-
-  const { isLoading, isSuccess } = useWaitForTransaction({ hash: data?.hash })
+  // const fetcher = useFetcher<typeof action>()
+  const fetcher = useFetcher<ActionFunctionArgs>()
+  const {
+    txHash,
+    isTransactionLoading,
+    isTransactionSuccess,
+    initiateTransaction,
+  } = useClientTransaction()
+  const [isTransactionInitiated, setIsTransactionInitiated] = useState(false)
 
   useEffect(() => {
-    if (!isLoading && isSuccess && data?.hash) {
-      setTxHash(data.hash)
-      setIsSubmitting(false) // Reset submitting state
+    if (txHash) {
+      // If the transaction hash is available, proceed with any post-transaction logic
+      // This could involve enabling the form submission button again
+      setIsTransactionInitiated(false)
     }
-  }, [isLoading, isSuccess, data?.hash])
+  }, [txHash])
 
-  const [form, { accountabilityAddress, pactDescription, userAddress }] =
-    useForm({
-      onValidate({ formData }) {
-        return parse(formData, { schema: validationSchema })
-      },
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    // let's prevent the default event
+    event.preventDefault()
 
-      shouldValidate: 'onBlur',
-      onSubmit(event, { submission }) {
-        event.preventDefault()
-        if (isSubmitting) {
-          console.log('Already submitting')
-          return
+    // grab the form element
+    let $form = event.currentTarget
+
+    // get the formData from that form
+    let formData = new FormData($form)
+
+    if (txHash) {
+      formData.set('txHash', txHash)
+    }
+
+    // and finally submit the form data, re-using the method and action from the form
+    // submit(formData, {
+    //   action: $form.getAttribute('action') ?? $form.action,
+    // })
+  }
+  // const { config } = usePrepareSendTransaction({
+  //   to: '0x04EA475026a0AB3e280F749b206fC6332E6939F1',
+  //   value: parseEther('0.000001'),
+  // })
+  // const { data: transactionData, sendTransaction } = useSendTransaction(config)
+  // const { isLoading, isSuccess } = useWaitForTransaction({
+  //   hash: transactionData?.hash,
+  // })
+
+  // useEffect(() => {
+  //   if (isSuccess && transactionData?.hash) {
+  //     console.log('Transaction successful:', transactionData.hash)
+  //     // Create a new FormData instance and append the txHash
+  //     const formData = new FormData()
+
+  //     formData.append('txHash', transactionData.hash)
+  //     // Use the fetcher to submit the formData to your action
+  //     fetcher.submit(formData, {
+  //       method: 'post',
+  //     })
+  //   }
+  // }, [transactionData, isSuccess])
+
+  const [form, { userAddress }] = useForm({
+    onValidate({ formData }) {
+      return parse(formData, { schema: validationSchema })
+    },
+
+    shouldValidate: 'onBlur',
+    onSubmit: async (event, { submission }) => {
+      event.preventDefault()
+      console.log('submission', submission)
+      const config = await prepareSendTransaction({
+        to: '0x04EA475026a0AB3e280F749b206fC6332E6939F1',
+        value: parseEther('0.000001'),
+      })
+      const { hash } = await sendTransaction(config)
+
+      if (hash) {
+        const { transactionHash, status } = await waitForTransaction({
+          hash: hash,
+        })
+
+        if (status === 'success') {
+          console.log('tx hash', transactionHash)
         }
-        if (submission.intent && txHash === null) {
-          // add in the client side tx, and get the txHash
-          setIsSubmitting(true)
-          console.log('Creating pact:', submission.payload)
-          sendTransaction?.()
-        }
-      },
-    })
-
-  useEffect(() => {
-    if (txHash && !isSubmitting) {
-      const formData = new FormData()
-      formData.append('userAddress', wallet)
-      formData.append('txHash', txHash)
-
-      for (const value of formData.values()) {
-        console.log(value)
       }
-      // Submit the form data along with the transaction hash
-      // You can use fetcher or a traditional form submission here
-      fetcher.submit(formData, { method: 'post' })
-    }
-  }, [txHash, isSubmitting])
+
+      // if (!txHash && !isTransactionInitiated) {
+      //   setIsTransactionInitiated(true)
+      //   await initiateTransaction() // This will set the txHash once the transaction is done
+      //   setIsTransactionInitiated(true) // Prevents multiple transactions
+      // } else if (txHash) {
+      //   // Now you have the txHash, submit your form data along with the txHash
+      //   const formData = new FormData(event.currentTarget)
+      //   formData.append('txHash', txHash)
+      //   console.log('txHash', txHash)
+      //   // ... submit the form data to your server using fetcher or a direct POST request ...
+      // }
+    },
+  })
 
   return (
     <Card className="w-full pb-8 pt-4">
       <div className="space-y-4">
-        <fetcher.Form {...form.props} className="flex flex-col gap-4 p-6">
+        <form
+          {...form.props}
+          // onSubmit={handleSubmit}
+          className="flex flex-col gap-4 p-6"
+        >
           <Input
             {...conform.input(userAddress)}
             type="hidden"
             name="userAddress"
             value={wallet}
           />
-          <div className="flex flex-col gap-2">
+          {/* <div className="flex flex-col gap-2">
             <Label className="m-x-auto text-sm text-foreground">
               Accountability Address
             </Label>
@@ -184,19 +252,13 @@ export function CreatePactForm() {
             <span className="flex items-center text-xs font-medium tracking-wide text-red-500">
               {pactDescription.error}
             </span>
-          </div>
-          <Button
-            variant="outline"
-            size="sm"
-            className="block w-full"
-            type="submit"
-            value="create-pact-with-transaction"
-          >
-            {isLoading ? 'Creating Pact' : 'Create Pact'}
+          </div> */}
+          <Button variant="outline" size="sm" className="block w-full">
+            {isTransactionLoading ? 'Creating Pact' : 'Create Pact'}
           </Button>
-        </fetcher.Form>
-        <p>txHash: {data?.hash}</p>
+        </form>
       </div>
+      {isTransactionSuccess && <p>tx hash: {txHash}</p>}
     </Card>
   )
 }
